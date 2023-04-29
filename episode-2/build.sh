556e65b6d917059d07f794bd5f7b6bca3ed561f7
TheCazzXz <65710413+TheCrazzXz@users.noreply.github.com>
2023-04-29 19:47:35 -0400

    Update build.sh

--- episode-2/build.sh
+++ episode-2/build.sh
@@ -1,3 +1,4 @@
+#/bin/sh
 g++ main.cpp glad/glad.c -o game -std=c++11 -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
 
 if [ $? -eq 0 ]; then
