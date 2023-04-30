37bd2c8dbbe492bb1ec0e863fcbee8c37c9097cd
TheCazzXz <65710413+TheCrazzXz@users.noreply.github.com>
2023-04-29 21:30:18 -0400

    Add files via upload

---
+++ episode-5/StaticShader.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef __STATIC_SHADER_H__
+#define __STATIC_SHADER_H__
+
+#define VERTEX_PATH "vertexShader"
+#define FRAGMENT_PATH "fragmentShader"
+
+#include "ShaderProgram.h"
+
+class StaticShader : public ShaderProgram
+{
+public:
+	StaticShader()
+		:	ShaderProgram(VERTEX_PATH, FRAGMENT_PATH)
+	{
+
+	}
+
+protected:
+	void bindAttributes()
+	{
+		bindAttribute(0, "position");
+	}
+};
+
+#endif 
---
+++ episode-5/main.cpp
@@ -0,0 +1,126 @@
+
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include "RawModel.h"
+#include "Loader.h"
+#include "Renderer.h"
+#include "ShaderProgram.h"
+#include "StaticShader.h"
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    StaticShader shader;
+
+    Loader loader;
+    Renderer renderer;
+
+
+  float vertices[] = {
+    -0.5, 0.5, 0,
+    -0.5, -0.5, 0,
+
+    0.5, -0.5, 0,
+    0.5, 0.5, 0
+  };
+  unsigned int indices[] = { 0, 1, 3, 3, 1, 2 };
+    SizedArray<float> verticesArr(vertices, sizeof(vertices));
+    SizedArray<unsigned int> indicesArr(indices, sizeof(indices));
+
+
+
+    RawModel model = loader.loadToVAO(verticesArr, indicesArr);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        renderer.prepare();
+
+        // render
+        // ------
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
+        shader.start();
+        renderer.render(model);
+        shader.stop();
+        //glUseProgram(shaderProgram);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+
+    }
+    loader.cleanUP();
+    shader.cleanUp();
+    //glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
